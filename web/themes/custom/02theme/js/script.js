49a328f869b77789df1b5f261101311478d696fe
nhunglele2492 <nhung.le@ffwagency.com>
2018-03-16 22:01:40 +0700

    Add theme

---
+++ web/themes/custom/02theme/js/script.js
@@ -0,0 +1,12 @@
+/*jslint browser: true*/
+/*global $, jQuery, Modernizr, enquire*/
+(function (window, document, $, Drupal) {
+  var $html = $('html'),
+    mobileOnly = "screen and (max-width:47.9375em)", // 767px.
+    mobileTabletOnly = "screen and (max-width: 63.9375em)", // 1023px.
+    mobileLandscape = "(min-width:30em)", // 480px.
+    tablet = "(min-width:48em)"; // 768px.
+    desktop = "(min-width: 64em)"; // min-width: 1024px.
+  // Add  functionality here.
+
+}(this, this.document, this.jQuery, Drupal));
