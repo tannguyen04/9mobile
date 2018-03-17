99ab88f7693a8b33b6be7da5f1065ee9afe33a7b
Tan Nguyen <raucaixanh04@gmail.com>
2018-03-17 12:43:41 +0700

    Update deploy.sh

--- scripts/gcloud/deploy.sh
+++ scripts/gcloud/deploy.sh
@@ -6,7 +6,7 @@ ssh -o "StrictHostKeyChecking no" raucaixanh04@35.186.148.73 << EOF
   composer install
   cd web && drush updb -y
   drush cim --source=../config -y
-  cd themes/custom/spectre/pattern-lab && composer install && php core/console --generate
+  cd themes/custom/spectre/.npm && npm install && gulp build-pattern-lab && gulp pl-generate
   drush cc all
   exit
 EOF
