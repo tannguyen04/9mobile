ac96896a6690a460e76d401f4a82c440aca99fc5
Tan Nguyen <raucaixanh04@gmail.com>
2018-03-17 10:49:26 +0700

    Update deploy.sh

--- scripts/gcloud/deploy.sh
+++ scripts/gcloud/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-ssh -o "StrictHostKeyChecking no" raucaixanh04@35.186.148.73
-cd /var/www/html/9mobile.vn
-git pull origin dev
-composer install
-cd web && drush updb -y
-cd themes/custom/spectre/pattern-lab && composer install && php core/console --generate
-exit
+ssh -o "StrictHostKeyChecking no" raucaixanh04@35.186.148.73 << EOF
+  cd /var/www/html/9mobile.vn
+  git pull origin dev
+  composer install
+  cd web && drush updb -y
+  cd themes/custom/spectre/pattern-lab && composer install && php core/console --generate
+  exit
+EOF
