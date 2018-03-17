eb0341f052ec024ff7c2036db769aaa4564ad6b9
Tan Nguyen <raucaixanh04@gmail.com>
2018-03-17 10:52:29 +0700

    Update deploy.sh

--- scripts/gcloud/deploy.sh
+++ scripts/gcloud/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ssh -o "StrictHostKeyChecking no" raucaixanh04@35.186.148.73 << EOF
   cd /var/www/html/9mobile.vn
+  git checkout dev
   git pull origin dev
   composer install
   cd web && drush updb -y
