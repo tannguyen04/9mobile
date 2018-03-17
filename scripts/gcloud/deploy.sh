642cdebf30f14c436de14ef56b27b64cad148776
Tan Nguyen <raucaixanh04@gmail.com>
2018-03-17 13:43:28 +0700

    Update deploy.sh

--- scripts/gcloud/deploy.sh
+++ scripts/gcloud/deploy.sh
@@ -3,7 +3,9 @@ ssh -o "StrictHostKeyChecking no" raucaixanh04@35.186.148.73 << EOF
   cd /var/www/html/9mobile.vn
   git fetch origin
   git checkout dev
-  git merge -X theirs origin/dev
+  git clean -df web/themes/custom/spectre/
+  git reset --hard
+  git merge origin/dev
   composer install
   cd web && drush updb -y
   drush cim --source=../config -y
