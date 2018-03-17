30ee44a6f77574da1aa412084bf4dbab6e04dfb6
Tan Nguyen <raucaixanh04@gmail.com>
2018-03-17 11:36:46 +0700

    Deploy: Import config

--- scripts/gcloud/deploy.sh
+++ scripts/gcloud/deploy.sh
@@ -5,7 +5,7 @@ ssh -o "StrictHostKeyChecking no" raucaixanh04@35.186.148.73 << EOF
   git pull origin dev
   composer install
   cd web && drush updb -y
-  drush cim --source=../config
+  drush cim --source=../config -y
   cd themes/custom/spectre/pattern-lab && composer install && php core/console --generate
   drush cc all
   exit
