f4f35690fee5e390bc05b6c1ca9216b2dcc96fe9
Tan Nguyen <raucaixanh04@gmail.com>
2018-03-17 10:27:21 +0700

    Update deploy.sh

--- scripts/gcloud/deploy.sh
+++ scripts/gcloud/deploy.sh
@@ -1,1 +1,7 @@
-acbc
+#!/bin/bash
+cd /var/www/html/9mobile.vn
+git pull origin dev
+composer install
+cd web && drush updb -y
+cd themes/custom/spectre/pattern-lab && composer install && php core/console --generate
+exit
