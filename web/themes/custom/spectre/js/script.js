10ef5e4d754bd8ed04ad5826d62b2e147a5c27bb
nhunglele2492 <nhung.le@ffwagency.com>
2018-03-20 14:29:52 +0700

    Add carousel

--- web/themes/custom/spectre/js/script.js
+++ web/themes/custom/spectre/js/script.js
@@ -1,12 +1,35 @@
 /*jslint browser: true*/
 /*global $, jQuery, Modernizr, enquire*/
 (function (window, document, $, Drupal) {
+    "use strict";
   var $html = $('html'),
     mobileOnly = "screen and (max-width:47.9375em)", // 767px.
     mobileTabletOnly = "screen and (max-width: 63.9375em)", // 1023px.
     mobileLandscape = "(min-width:30em)", // 480px.
-    tablet = "(min-width:48em)"; // 768px.
+    tablet = "(min-width:48em)", // 768px.
     desktop = "(min-width: 64em)"; // min-width: 1024px.
   // Add  functionality here.
+  console.log('test');
 
-}(this, this.document, this.jQuery, Drupal));
+  $(".js-carousel").slick({
+    autoplay: true,
+    dots: true,
+    customPaging: function(slick,index) {
+      var pager = $(slick.$slides[index]).find('img').data('thumb');
+      return '<a><img src="'+pager+'"></a>';
+    },
+
+    responsive: [{
+        breakpoint: 500,
+        settings: {
+            dots: false,
+            arrows: false,
+            infinite: false,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        }
+    }]
+  });
+
+
+}(this, this.document, this.jQuery, this.Drupal));
